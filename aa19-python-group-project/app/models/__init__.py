8ee4663d6dc809b45e19650eebccbd6a76ec80fe
Sean <Kwasnicki.Sean@gmail.com>
2024-05-28 21:55:00 -0700

    init

--- aa19-python-group-project/app/models/__init__.py
+++ aa19-python-group-project/app/models/__init__.py
@@ -3,3 +3,5 @@ from .user import User
 from .db import environment, SCHEMA
 from .server import Server
 from .server_member import ServerMember
+from .channel import Channel
+from .channelmembers import ChannelMembers
