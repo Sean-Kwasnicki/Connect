99af33d69d53c6871e42c6be661998cf9859bf80
Eli <eli.suffridge@gmail.com>
2024-05-30 21:53:14 -0700

    almost done with all of the server routes

---
+++ aa19-python-group-project/app/forms/create_server_form.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, ValidationError
+from app.models import Server
+
+
+def unique_name(form, field):
+
+    name = form.data['name']
+    server = Server.query.filter(Server.name == name).first()
+    if server:
+        raise ValidationError('Server all ready exist with that name')
+
+
+class CreateServerForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), unique_name])
