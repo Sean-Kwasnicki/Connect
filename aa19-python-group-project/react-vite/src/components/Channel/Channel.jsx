1aa8047ca3f8a942b47a99dda44a8727d00246e0
Wei Cheng Chen <DrChenWeiCheng@gmail.com>
2024-06-05 19:51:30 -0700

    IGNORE THIS BRANCH

--- aa19-python-group-project/react-vite/src/components/Channel/Channel.jsx
+++ aa19-python-group-project/react-vite/src/components/Channel/Channel.jsx
@@ -1,33 +1,86 @@
 import { Outlet, useParams } from "react-router-dom";
-import { getMessagesThunk } from "../../redux/message";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { getMessagesThunk, createMessageThunk } from "../../redux/message";
+import io from "socket.io-client";
 import s from "./Channel.module.css";
+
+const socket = io.connect("/");
 
 const Channel = () => {
   const { channelId } = useParams();
   const dispatch = useDispatch();
-
+  const user = useSelector((state) => state.session.user);
   const messages = useSelector((state) => state.messages.messages);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     dispatch(getMessagesThunk(channelId));
   }, [dispatch, channelId]);
 
-  console.log(messages)
+  useEffect(() => {
+    socket.on("message", (data) => {
+      dispatch(getMessagesThunk(channelId));
+    });
+
+    return () => {
+      socket.off("message");
+    };
+  }, [dispatch, channelId]);
+
+  const joinRoom = () => {
+    if (channelId) {
+      socket.emit("join", { room: channelId });
+    }
+  };
+
+  const leaveRoom = () => {
+    if (channelId) {
+      socket.emit("leave", { room: channelId });
+    }
+  };
+
+  useEffect(() => {
+    joinRoom();
+    return leaveRoom;
+  }, [channelId]);
+
+  const sendMessage = async () => {
+    if (message && channelId) {
+      await dispatch(createMessageThunk(channelId, { content: message }));
+      socket.emit("message", {
+        message: { user: user.username, content: message },
+        room: channelId,
+      });
+      setMessage("");
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendMessage();
+  };
 
   return (
     <>
       <ul className={s.channels}>
-        {messages.map(({ user, content, id }) => {
-          return (
-            <li key={id} className={s.message}>
-              <span>{user}</span>
-              <p>{content}</p>
-            </li>
-          );
-        })}
+        {messages.map(({ user, content, id }) => (
+          <li key={id} className={s.message}>
+            <span>{user}</span>
+            <p>{content}</p>
+          </li>
+        ))}
       </ul>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </label>
+        <button type="submit">Send Message</button>
+      </form>
       <Outlet />
     </>
   );
--- aa19-python-group-project/react-vite/src/redux/message.js
+++ aa19-python-group-project/react-vite/src/redux/message.js
@@ -1,35 +1,61 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// Action Types
 const GET_MESSAGES = "messages/getMessages";
+const CREATE_MESSAGE = "messages/createMessage";
 
-///////////////////////////////////////////////////////
+// Actions
+const getMessages = (messages) => ({
+  type: GET_MESSAGES,
+  payload: messages,
+});
 
-const getMessages = (messages) => {
-  return {
-    type: GET_MESSAGES,
-    payload: messages,
-  };
-};
+const createMessage = (message) => ({
+  type: CREATE_MESSAGE,
+  payload: message,
+});
 
-///////////////////////////////////////////////////////
-
+// Thunks
 export const getMessagesThunk = (channelId) => async (dispatch) => {
-  const response = await fetch(`/api/channels/${channelId}/messages`);
-  if (response.ok) {
-    const messages = await response.json();
-    dispatch(getMessages(messages));
+  try {
+    const response = await axios.get(`/api/channels/${channelId}/messages`);
+    if (response.status === 200) {
+      const messages = response.data;
+      dispatch(getMessages(messages));
+    }
+  } catch (error) {
+    console.error("Failed to fetch messages:", error);
   }
 };
 
-///////////////////////////////////////////////////////
+export const createMessageThunk = (channelId, content) => async (dispatch) => {
+  try {
+    const response = await axios.post(`/api/channels/${channelId}/messages`, { content });
+    if (response.status === 201) {
+      const newMessage = response.data;
+      dispatch(createMessage(newMessage));
+    }
+  } catch (error) {
+    console.error("Failed to create message:", error);
+  }
+};
 
-const initialState = { messages: [] };
+// Initial State
+const initialState = {
+  messages: [],
+};
 
-function messageReducer(state = initialState, action) {
+// Reducer
+const messageReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_MESSAGES:
-      return { ...state, messages: [...action.payload] };
+      return { ...state, messages: action.payload };
+    case CREATE_MESSAGE:
+      return { ...state, messages: [action.payload, ...state.messages] };
     default:
       return state;
   }
-}
+};
 
 export default messageReducer;
