337ffb9a273d1db085a3572f3569cbba91491fbd
Eli <eli.suffridge@gmail.com>
2024-06-08 10:55:27 -0700

    starting loginform page css but forgot to swap branches

--- aa19-python-group-project/react-vite/src/components/LoginFormPage/LoginFormPage.jsx
+++ aa19-python-group-project/react-vite/src/components/LoginFormPage/LoginFormPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { thunkLogin } from "../../redux/session";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, Navigate, useNavigate } from "react-router-dom";
-import "./LoginForm.css";
+import s from "./LoginForm.module.css";
 
 function LoginFormPage() {
   const navigate = useNavigate();
@@ -32,35 +32,42 @@ function LoginFormPage() {
   };
 
   return (
-    <>
-      <h1>Log In</h1>
-      {errors.length > 0 &&
-        errors.map((message) => <p key={message}>{message}</p>)}
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.password && <p>{errors.password}</p>}
-        <button type="submit">Log In</button>
-      </form>
-      <NavLink to="/signup">Signup</NavLink>
-    </>
+    <div className={s.login_form_page_container}>
+      <div className={s.login_form_page}>
+        <h1 className={s.welcome_back}>Welcome back</h1>
+        <h3 className={s.see_you_again}>
+          {"We're"} so exited to see you again!
+        </h3>
+        {errors.length > 0 &&
+          errors.map((message) => <p key={message}>{message}</p>)}
+        <form onSubmit={handleSubmit}>
+          <label>
+            EMAIL <span>*</span>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </label>
+          {errors.email && <p>{errors.email}</p>}
+          <label>
+            PASSWORD <span>*</span>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          {errors.password && <p>{errors.password}</p>}
+          <button type="submit">Log In</button>
+        </form>
+        <div>
+          Need an account? <NavLink to="/signup">Signup</NavLink>
+        </div>
+      </div>
+    </div>
   );
 }
 
