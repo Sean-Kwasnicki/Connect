1e0840c4ba3abe37398172d325f5c90ab16c5c3f
Eli <eli.suffridge@gmail.com>
2024-06-15 20:17:44 -0700

    added static path back

--- aa19-python-group-project/app/__init__.py
+++ aa19-python-group-project/app/__init__.py
@@ -20,7 +20,7 @@ from .seeds import seed_commands
 from .config import Config
 # from .sockets import socketio
 
-app = Flask(__name__)
+app = Flask(name, static_folder='../react-vite/dist', static_url_path='/')
 
 # Setup login manager
 login = LoginManager(app)
