b7e8b8fbc3bc99bb00b0b715b656b6d60b1cfe34
Eli <eli.suffridge@gmail.com>
2024-06-15 19:29:17 -0700

    moved monkey_patch as first import

--- aa19-python-group-project/app/__init__.py
+++ aa19-python-group-project/app/__init__.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 import os
 from flask import Flask, render_template, request, session, redirect
 from flask_cors import CORS
@@ -88,9 +91,7 @@ def not_found(e):
 # socket stuff
 from flask_socketio import SocketIO, emit, join_room, leave_room,send
 
-import eventlet
 
-eventlet.monkey_patch()
 # Initialize SocketIO
 socketio = SocketIO()
 
