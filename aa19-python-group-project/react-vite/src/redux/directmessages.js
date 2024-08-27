29ceb397eae4cdf189279f53ac7822c44b84e1a3
Sean <Kwasnicki.Sean@gmail.com>
2024-08-26 20:31:04 -0700

    updates

--- aa19-python-group-project/react-vite/src/redux/directmessages.js
+++ aa19-python-group-project/react-vite/src/redux/directmessages.js
@@ -7,25 +7,7 @@ export const fetchDirectMessages = createAsyncThunk('directMessages/fetchDirectM
     return response.data.DirectMessages;
 });
 
-export const createDirectMessage = createAsyncThunk(
-    'directMessages/createDirectMessage',
-    async ({ receiverId, content }) => {
-        const response = await fetch(`/api/direct_messages/${receiverId}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ content })
-        });
 
-        if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(`Request failed with status ${response.status}: ${errorData.error}`);
-        }
-
-        return await response.json();
-    }
-);
 
 // Slice
 const directMessagesSlice = createSlice({
