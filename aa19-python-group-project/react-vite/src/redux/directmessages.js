c8cde3408d27da8b66e4f85150b127b0b6b6da83
Sean <Kwasnicki.Sean@gmail.com>
2024-08-26 18:54:32 -0700

    update

--- aa19-python-group-project/react-vite/src/redux/directmessages.js
+++ aa19-python-group-project/react-vite/src/redux/directmessages.js
@@ -7,23 +7,12 @@ export const fetchDirectMessages = createAsyncThunk('directMessages/fetchDirectM
     return response.data.DirectMessages;
 });
 
-export const createDirectMessage = createAsyncThunk(
-    'directMessages/createDirectMessage',
-    async ({ receiverId, content }) => {
-        const response = await axios.post(
-            `/api/direct_messages/${receiverId}`,
-            { content },
-            {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-        );
-        return response.data;
-    }
-);
+export const createDirectMessage = createAsyncThunk('directMessages/createDirectMessage', async ({ receiverId, content }) => {
+    const response = await axios.post(`/api/direct_messages`, { receiver_id: receiverId, content });
+    return response.data;
+});
 
-//Slice
+// Slice
 const directMessagesSlice = createSlice({
     name: 'directMessages',
     initialState: [],
